5f9e0a90331fe33cc5d82aea25a33497e97cca6d
couch Janus <couchjanus@gmail.com>
2020-01-10 13:08:43 +0200

    add secure asset

--- resources/views/layouts/partials/_scripts.blade.php
+++ resources/views/layouts/partials/_scripts.blade.php
@@ -1,4 +1,4 @@
 <!-- Bootstrap core JavaScript -->
-<script src="{{ asset('js/jquery.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('js/shop.js') }}"></script>
+<script src="{{ secure_asset('js/jquery.min.js') }}"></script>
+<script src="{{ secure_asset('js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ secure_asset('js/shop.js') }}"></script>
